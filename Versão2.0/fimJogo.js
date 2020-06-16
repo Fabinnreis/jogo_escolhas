1b0dbe2cab451e1a04ccd374f62e08b2e7e8636d
Fabinnreis <fabioreisti@outlook.com>
2020-06-16 12:24:29 -0300

    Delete fimJogo.js

--- Versão2.0/fimJogo.js
+++
@@ -1,5 +0,0 @@
-function fimDeJogo(){
-    alert("Ocorre o duelo final entre Yone e Shaka. O Samurai vence, liberta sua família, volta para sua vila natal e é honrado com elevação de grau no seu dojo. Virando assim um herói Samurai.");
-    alert("Parabéns você zerou o jogo!");
-    introducao();
-}
